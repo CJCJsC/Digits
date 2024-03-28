1bcd102982e488470164915d887a3ad3b4bc6f14
CJ Caraang <156409258+CJCJsC@users.noreply.github.com>
2024-03-28 13:30:50 -1000

    Round 1

--- app/imports/ui/components/ContactsAdmin.jsx
+++ app/imports/ui/components/ContactsAdmin.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Image } from 'react-bootstrap';
+import * as Proptypes from 'prop-types';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const Contacts = ({ contact }) => (
+const ContactAdmin = ({ contact }) => (
   <Card>
     <Card.Header>
       <Image src={contact.image} width={75} />
@@ -12,19 +13,21 @@ const Contacts = ({ contact }) => (
     </Card.Header>
     <Card.Body>
       <Card.Text>{contact.description}</Card.Text>
+      <footer className="blockquote-footer">{contact.owner}</footer>
     </Card.Body>
   </Card>
 );
 
 // Require a document to be passed to this component.
-Contacts.propTypes = {
+ContactAdmin.propTypes = {
   stuff: PropTypes.shape({
     name: PropTypes.string,
     quantity: PropTypes.number,
     condition: PropTypes.string,
     _id: PropTypes.string,
     description: Proptypes.string,
+    owner: PropTypes.string,
   }).isRequired,
 };
 
-export default Contacts;
+export default ContactAdmin;
